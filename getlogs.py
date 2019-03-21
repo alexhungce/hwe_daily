92269ceae8117362ea9f9831f046c2d4ff0ce4c7
Alex Hung <alex.hung@canonical.com>
2019-03-20 19:26:14 -0700

    getlogs: add a tool to download attachments from a LP bug
    Signed-off-by: Alex Hung <alex.hung@canonical.com>

---
+++ getlogs.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+# Copyright (C) 2019 Canonical
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+import sys, os, launchpadlib
+from launchpadlib.launchpad import Launchpad
+
+cachedir = os.environ['HOME'] + "/.launchpadlib/cache/"
+
+def no_credential():
+    print("Can't proceed without Launchpad credential.")
+    sys.exit()
+
+launchpad = Launchpad.login_with('Daily_track', 'production', credential_save_failed=no_credential, version='devel')
+
+def main():
+
+    myself = launchpad.me
+    bugs_assigned = myself.searchTasks(assignee = myself)
+
+    bug = launchpad.bugs[sys.argv[1]]
+
+    print("Get logs for %s | %s" % (bug.web_link, bug.title))
+
+    attachs = bug.attachments
+    for attach in attachs:
+        print("Downloading " + attach.title + "...")
+
+        with open(attach.title, 'wb+') as f:
+            f.write(attach.data.open().getvalue())
+            f.close()
+
+    return 0
+
+if __name__ == "__main__":
+    main()
